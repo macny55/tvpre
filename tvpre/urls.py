10a73caf0dde0d50da3258b89c8bd408a91d5567
macny55 <mcgradyrockets2004@gmail.com>
2015-03-03 09:18:21 +0900

    Update urls.py

--- tvpre/urls.py
+++ tvpre/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-from tvpre.views import top_page
+from tvpre.views import top_page, nhk_program
 
 urlpatterns = patterns('',
+    (r'^programs/$', nhk_program),
     (r'^$', top_page),
 )
